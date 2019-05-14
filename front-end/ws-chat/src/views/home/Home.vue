28f8a42c0e8e7fc0890d38ac0b12a9f0fd20e9b1
damao2250 <15678565370@163.com>
2019-05-15 01:15:12 +0800

    updata front-end

---
+++ front-end/ws-chat/src/views/home/Home.vue
@@ -0,0 +1,50 @@
+<template>
+  <el-container>
+    <el-aside width="200px">
+      <el-row>
+        <el-col :span="24">
+          <div class="grid-content contacts_search">1</div>
+        </el-col>
+      </el-row>
+      <div>
+        <el-row>
+          <el-col :span="24">
+            <div class="grid-content">2</div>
+          </el-col>
+        </el-row>
+      </div>
+    </el-aside>
+    <el-main>
+      聊天内容
+    </el-main>
+  </el-container>
+</template>
+<script>
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.el-container {
+  height: 100%;
+}
+.el-aside {
+  background: #e6e6e6;
+}
+.el-main {
+  color: #f5f5f5;
+}
+.grid-content {
+  border-radius: 4px;
+  min-height: 56px;
+  padding:10px;
+  
+}
+.contacts_search {
+  background: #eeebe9;
+}
+</style>
+
